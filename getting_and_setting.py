89895e87b1acbfa03b770c82fcffb4c8389f21f9
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-03-24 20:31:17 -0300

    Pegando e modificando um quadrado de 100 pixels

--- getting_and_setting.py
+++ getting_and_setting.py
@@ -13,3 +13,10 @@ print "Pixel at (0,0)  - Red: %d, Green: %d, Blue: %d" % (r, g, b)
 image[0, 0] = (0, 0, 255)
 (b, g, r) = image[0, 0]
 print "Pixel at (0,0)  - Red: %d, Green: %d, Blue: %d" % (r, g, b)
+
+corner = image[0:100, 0:100]
+cv2.imshow("Corner", corner)
+image[0:100, 0:100] = (0, 255, 0)
+
+cv2.imshow("Updated", image)
+cv2.waitKey(0)
