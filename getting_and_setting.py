c149b150595182e9ff65a25d8bcc86ed3c6187f6
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-03-24 20:22:18 -0300

    Alterando o valor de um Pixel

--- getting_and_setting.py
+++ getting_and_setting.py
@@ -9,3 +9,7 @@ image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 (b, g, r) = image[0, 0]
 print "Pixel at (0,0)  - Red: %d, Green: %d, Blue: %d" % (r, g, b)
+
+image[0, 0] = (0, 0, 255)
+(b, g, r) = image[0, 0]
+print "Pixel at (0,0)  - Red: %d, Green: %d, Blue: %d" % (r, g, b)
