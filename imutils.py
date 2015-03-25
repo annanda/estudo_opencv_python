5b51e378f8ff5d7db3c8058e76553664e9c7c68a
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-03-25 19:54:22 -0300

    melhorando o codigo

--- imutils.py
+++ imutils.py
@@ -1,17 +1,18 @@
 import numpy as np
 import cv2
 
+
 def translate(image, x, y):
 
+    # matriz de translacao tem que ser do tipo float
+    # na primeira linha o ultimo numero diz quantos px vao sofrer o shift
+    # pra direita ou esquerda (valores positivos - direita, valores negativos -
+    # esquerda
+    # na segunda linha, ultimo numero diz quantos px vao sofrer o shift
+    # pra cima ou baixo (valores positivos - baixo, valores negativos - cima)
     M = np.float32([[1, 0, x], [0, 1, y]])
-# matriz de translacao tem que ser do tipo float
-# na primeira linha o ultimo numero diz quantos px vao sofrer o shift
-# pra direita ou esquerda (valores positivos - direita, valores negativos -
-# esquerda
-# na segunda linha, ultimo numero diz quantos px vao sofrer o shift
-# pra cima ou baixo (valores positivos - baixo, valores negativos - cima)
+    # funcao que faz a translacao, imagem a ser transformada, matriz de traslacao,
+    # altura da imagem a ser modificada,
+    # largura da imagem a ser modificada
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
-# funcao que faz a translacao, imagem a ser transformada, matriz de traslacao,
-# altura da imagem a ser modificada,
-# largura da imagem a ser modificada
     return shifted
