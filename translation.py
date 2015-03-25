641fe575e1aed0667ebcb3533ed61324f7f28fc5
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-03-25 17:54:25 -0300

    Transladando uma imagem para esquerda e para baixo

--- translation.py
+++ translation.py
@@ -11,7 +11,7 @@ image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
 M = np.float32([[1, 0, 25], [0, 1, 50]])
-# matriz de translacao
+# matriz de translacao tem que ser do tipo float
 # na primeira linha o ultimo numero diz quantos px vao sofrer o shift
 # pra direita ou esquerda (valores positivos - direita, valores negativos -
 # esquerda
@@ -22,4 +22,10 @@ shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
 # altura da imagem a ser modificada,
 # largura da imagem a ser modificada
 cv2.imshow("Shifted Down and Right", shifted)
+cv2.waitKey(0)
+
+M = np.float32([[1, 0, -40], [0, 1, -100]])
+# agora vai transladar para a esquerda e cima, pois tem numeros negativos
+shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+cv2.imshow("Shifted Up and Left", shifted)
 cv2.waitKey(0)
