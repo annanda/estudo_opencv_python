d988d7c7c41409c2bbd42ff9969205090d474282
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-03-25 19:26:16 -0300

    Adicionando o arquivo imutils que contem as funcoes basicas de transformacao de imagem

---
+++ imutils.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+
+def translate(image, x, y):
+    M = np.float32[[1, 0, x], [0, 1, y]]
+    shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+    return shifted
