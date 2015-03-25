26ceb0d846ed4357c4d8c4b23227e848eb928786
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-03-25 10:45:00 -0300

    Desenhando um retângulo preenchido de cor

--- drawing.py
+++ drawing.py
@@ -25,6 +25,12 @@ cv2.rectangle(canvas, (10, 10), (60, 60), green)
 cv2.imshow("Canvas", canvas)
 cv2.waitKey(0)
 
-cv2.rectangle(canvas, (50, 200), (200, 125), red, 5)
+cv2.rectangle(canvas, (50, 200), (200, 225), red, 5)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+blue = (255, 0, 0)
+cv2.rectangle(canvas, (200, 50), (225, 125), blue, -1)
+# com o argumente da espessura da linha em -1 ele preenche o retangulo
 cv2.imshow("Canvas", canvas)
 cv2.waitKey(0)
