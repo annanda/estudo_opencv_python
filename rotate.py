549002281c60aab394b3934ceb16379b2f5e2f0f
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-03-31 19:51:56 -0300

    Rotacionando uma imagem a graus negativos

--- rotate.py
+++ rotate.py
@@ -18,3 +18,8 @@ M = cv2.getRotationMatrix2D(center, 45, 1.0)
 rotated = cv2.warpAffine(image, M, (w, h))
 cv2.imshow("Rotated by 45 Degrees", rotated)
 cv2.waitKey(0)
+
+M = cv2.getRotationMatrix2D(center, -90, 1.0)
+rotated = cv2.warpAffine(image, M, (w,h))
+cv2.imshow("Rotated by -90 Degrees", rotated)
+cv2.waitKey(0)
