9b574af223eced73a649c5d31378d3b50a4e72a1
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-04-01 12:56:25 -0300

    Somando os pixels de uma imagem com uma matriz

--- arithmetic.py
+++ arithmetic.py
@@ -9,10 +9,17 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
-# soma e subtracao openCV
+# soma e subtracao de arrays openCV
 print "max of 255: " + str(cv2.add(np.uint8([200]), np.uint8([100])))
 print "min of 0: " + str(cv2.subtract(np.uint8([50]), np.uint8([100])))
 
-# soma e subtracao NumPy usando aritmetica modular
+# soma e subtracao NumPy de arrays usando aritmetica modular
 print "wrap around: " + str(np.uint8([200]) + np.uint8([100]))
 print "wrap around: " + str(np.uint8([50]) - np.uint8([100]))
+
+# import bpdb; bpdb.set_trace()
+# cria uma matriz que vai ser adicionada a minha imagem
+M = np.ones(image.shape, dtype="uint8") * 100
+added = cv2.add(image, M)
+cv2.imshow("Added", added)
+cv2.waitKey(0)
