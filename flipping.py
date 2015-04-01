f6b1b6fd451f2e6b68509fbd0db3e89e622e8d20
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-04-01 11:36:35 -0300

    Adicionando comentarios em flipping.py

--- flipping.py
+++ flipping.py
@@ -8,14 +8,17 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
+# 1 indica que o flip e em torno de Y
 flipped = cv2.flip(image, 1)
 cv2.imshow("Flipped Horizontally", flipped)
 cv2.waitKey(0)
 
+# 0 indica que o flip e em torno de X
 flipped = cv2.flip(image, 0)
 cv2.imshow("Flipped Vertically", flipped)
 cv2.waitKey(0)
 
+# -1 indica que o flip e em torno de ambos: X e Y
 flipped = cv2.flip(image, -1)
 cv2.imshow("Flipped Horizontally & Vertically", flipped)
 cv2.waitKey(0)
