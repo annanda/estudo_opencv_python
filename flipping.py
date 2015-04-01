1e828115000f1d344dccaa0b35b5e7f7f8fec6d4
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-04-01 11:34:24 -0300

    Espelhando imagem horizontamente e verticalmente

--- flipping.py
+++ flipping.py
@@ -15,3 +15,7 @@ cv2.waitKey(0)
 flipped = cv2.flip(image, 0)
 cv2.imshow("Flipped Vertically", flipped)
 cv2.waitKey(0)
+
+flipped = cv2.flip(image, -1)
+cv2.imshow("Flipped Horizontally & Vertically", flipped)
+cv2.waitKey(0)
