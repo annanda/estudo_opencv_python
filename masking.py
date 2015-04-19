baf64d1994136f0c556fd761a09ddc32dfbc8d82
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-04-19 14:27:24 -0300

    Aplicando uma máscara redonda no centro da imagem. Usando operação AND  bit a bit.

--- masking.py
+++ masking.py
@@ -8,6 +8,7 @@ args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
+cv2.waitKey(0)
 
 # criando a matriz com zeros do mesmo tamanho da imagem e com dados do tipo
 # inteiro sem sinal de 8 bits
@@ -25,5 +26,12 @@ cv2.waitKey(0)
 # Assim a mascara funciona
 ####
 masked = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Mask Applied to Image", masked)
+cv2.waitKey(0)
+
+mask = np.zeros(image.shape[:2], dtype="uint8")
+cv2.circle(mask, (cX, cY), 100, 255, -1)
+masked = cv2.bitwise_and(image, image, mask=mask)
+cv2.imshow("Mask", mask)
 cv2.imshow("Mask Applied to Image", masked)
 cv2.waitKey(0)
