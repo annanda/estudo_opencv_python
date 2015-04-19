fb5fdb42bf3acaacd45b7d3f20ec147b2350c6f7
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-04-19 14:56:36 -0300

    Uma forma alternativa de visualizar a divisao em canais de cores. Faz merge com dois dos canais 0 e só o que eu quero exibir ativo.

--- splitting_and_merging.py
+++ splitting_and_merging.py
@@ -1,3 +1,4 @@
+import numpy as np
 import argparse
 import cv2
 
@@ -18,3 +19,9 @@ merged = cv2.merge([B, G, R])
 cv2.imshow("Merged", merged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+zeros = np.zeros(image.shape[:2], dtype="uint8")
+cv2.imshow("Red", cv2.merge([zeros, zeros, R]))
+cv2.imshow("Green", cv2.merge([zeros, G, zeros]))
+cv2.imshow("Blue", cv2.merge([B, zeros, zeros]))
+cv2.waitKey(0)
