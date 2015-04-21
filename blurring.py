a5aaf1dfa1e5b0e3fc2f29edc675b62b8a218de3
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-04-21 18:33:30 -0300

    Preparando o codigo para aplicar o borrado na imagem

---
+++ blurring.py
@@ -0,0 +1,11 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+cv2.waitKey(0)
