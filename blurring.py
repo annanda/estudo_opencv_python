7956c9b779ec9882e26b16e905164aa9a5d2850e
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-05-01 15:57:13 -0300

    Fazendo um blur com convolução usando a média. Usando diferentes kernel de convolução. Para diferentes intensidades de borramento.

--- blurring.py
+++ blurring.py
@@ -9,3 +9,13 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 cv2.waitKey(0)
+
+# aplicando blur na imagem com kernel de 3x3, 5x5, 7x7
+# Quanto maior o kerner, mais borrada a imagem fica
+# Usando a media
+blurred = np.hstack([
+    cv2.blur(image, (3, 3)),
+    cv2.blur(image, (5, 5)),
+    cv2.blur(image, (7, 7))])
+cv2.imshow("Averaged", blurred)
+cv2.waitKey(0)
