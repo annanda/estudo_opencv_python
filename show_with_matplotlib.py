d150d4d469e0c36c770cc1515951ece214e8dd90
Annanda Dandi de Freitas Sousa <annanda.sousa@gmail.com>
2015-05-13 10:14:01 -0300

    Consertando a diferenca de leitura da imagem no OpenCV e Matplotlib

--- show_with_matplotlib.py
+++ show_with_matplotlib.py
@@ -8,6 +8,8 @@ ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
+b,g,r = cv2.split(image)
+image = cv2.merge([r,g,b])
 plt.imshow(image, cmap='gray', interpolation='bicubic')
 # plt.xticks([]), plt.yticks([])
 plt.show()
